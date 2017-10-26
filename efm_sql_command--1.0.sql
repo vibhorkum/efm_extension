5c01c395688af4a1756ff18ff5244927bfb33aad
root <vibhor.aim@gmail.com>
2017-10-26 14:41:50 +0000

    adding function for pgpool monitoring

--- efm_sql_command--1.0.sql
+++ efm_sql_command--1.0.sql
@@ -1,6 +1,7 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION efm_sql_command" to load this file. \quit
 
+REVOKE ALL ON SCHEMA efm_extension FROM PUBLIC;
 CREATE FUNCTION efm_cluster_status (TEXT)
     RETURNS SETOF TEXT
 AS 'MODULE_PATHNAME',
@@ -81,17 +82,184 @@ FROM
         KEY = 'nodes';
 
 
-CREATE OR REPLACE FUNCTION efm_extension.pg_is_in_recovery() 
-    RETURNS BOOLEAN
+/* adding efm pgpool monitoring capability functions */
+CREATE TYPE efm_extension.pool_status AS (
+    pool_pid INTEGER,
+    start_time TEXT,
+    pool_id INTEGER,
+    backend_id INTEGER,
+    DATABASE TEXT,
+    username TEXT,
+    create_time TEXT,
+    majorversion INTEGER,
+    minorversion INTEGER,
+    pool_counter INTEGER,
+    pool_backendpid INTEGER,
+    pool_connected INTEGER
+);
+
+REVOKE ALL ON TYPE efm_extension.pool_status FROM PUBLIC;
+
+CREATE TYPE efm_extension.pool_link_status AS (
+    link_name TEXT,
+    status    TEXT
+);
+
+
+REVOKE ALL ON TYPE efm_extension.pool_link_status FROM PUBLIC;
+
+CREATE TABLE efm_extension.pgpool_nodes(
+   hostname TEXT,
+   port INTEGER,
+   database TEXT,
+   username TEXT,
+   password bytea);
+
+REVOKE ALL ON TABLE efm_extension.pgpool_nodes FROM PUBLIC;
+
+CREATE OR REPLACE FUNCTION efm_extension.encrypt_efm(TEXT, TEXT)
+RETURNS bytea
+LANGUAGE SQL
+AS
+$FUNCTION$
+  SELECT encrypt($1::BYTEA,$2::BYTEA,'aes');
+$FUNCTION$;
+
+REVOKE ALL ON FUNCTION efm_extension.encrypt_efm(TEXT,TEXT) FROM PUBLIC;
+
+
+CREATE OR REPLACE FUNCTION efm_extension.get_efm(BYTEA,TEXT)
+RETURNS text
+LANGUAGE SQL
+AS
+$FUNCTION$
+  SELECT convert_from(decrypt($1,$2::BYTEA,'aes'),'SQL_ASCII');
+$FUNCTION$;
+
+REVOKE ALL ON FUNCTION efm_extension.get_efm(BYTEA,TEXT) FROM PUBLIC;
+
+CREATE OR REPLACE FUNCTION efm_extension.pgpool_link_exists(TEXT)
+RETURNS bool AS $$
+   SELECT COALESCE($1 = ANY (dblink_get_connections()), false)
+$$ LANGUAGE sql;
+
+REVOKE ALL ON FUNCTION efm_extension.pgpool_link_exists(TEXT) FROM PUBLIC;
+
+CREATE OR REPLACE FUNCTION efm_extension.get_pgpool_links() 
+RETURNS SETOF efm_extension.pool_link_status
+LANGUAGE SQL
+AS
+$FUNCTION$
+ SELECT
+            'pgpool_' || hostname AS conn_name,
+            CASE WHEN efm_extension.pgpool_link_exists ('pgpool_' || hostname) THEN
+                'OK'
+            ELSE
+                dblink_connect('pgpool_' || hostname,
+                    format('hostaddr=%s port=%s dbname=%I user=%I password=%I',
+                        hostname,
+                        port,
+                        DATABASE,
+                        username,
+                        efm_extension.get_efm (PASSWORD,
+                            'efm')))
+        END
+    FROM
+        efm_extension.pgpool_nodes;
+$FUNCTION$;
+
+REVOKE ALL ON FUNCTION efm_extension.get_pgpool_links() FROM PUBLIC;
+
+CREATE OR REPLACE FUNCTION efm_extension.pgpool_backendpid_details (TEXT, INTEGER)
+    RETURNS SETOF efm_extension.pool_status
     LANGUAGE SQL
 AS $FUNCTION$
 SELECT
-    CASE WHEN inet_client_addr() = inet_server_addr() THEN
-        pg_catalog.pg_is_in_recovery()
-    ELSE
-        TRUE
+    *
+FROM
+    dblink($1,
+        'SHOW pool_pools') foo (pool_pid INTEGER,
+        start_time TEXT,
+        pool_id INTEGER,
+        backend_id INTEGER,
+        DATABASE TEXT,
+        username TEXT,
+        create_time TEXT,
+        majorversion INTEGER,
+        minorversion INTEGER,
+        pool_counter INTEGER,
+        pool_backendpid INTEGER,
+        pool_connected INTEGER)
+WHERE
+    pool_backendpid <> 0 AND pool_backendpid = $2;
+$FUNCTION$;
+
+
+REVOKE ALL ON FUNCTION efm_extension.pgpool_backendpid_details(TEXT, INTEGER) FROM PUBLIC;
+
+CREATE OR REPLACE FUNCTION efm_extension.add_pgpool_monitoring(TEXT, INTEGER, TEXT, TEXT, TEXT)
+RETURNS boolean
+LANGUAGE plpgsql
+AS
+$FUNCTION$
+DECLARE 
+  cmd_string TEXT;
+  server_name TEXT ;
+BEGIN
+    INSERT INTO efm_extension.pgpool_nodes 
+      VALUES($1,$2,$3,$4,efm_extension.encrypt_efm($5,'efm'));
+    RETURN TRUE;
+  EXCEPTION 
+        WHEN OTHERS THEN
+        RETURN FALSE;
 END;
 $FUNCTION$;
+
+REVOKE ALL ON FUNCTION efm_extension.add_pgpool_monitoring(TEXT, INTEGER, TEXT, TEXT, TEXT) FROM PUBLIC;
+
+CREATE OR REPLACE FUNCTION efm_extension.pg_is_in_recovery()
+    RETURNS BOOLEAN
+    LANGUAGE plpgsql
+AS $FUNCTION$
+DECLARE
+    recovery_status BOOLEAN := false;
+BEGIN
+    WITH pid_info AS (
+    SELECT
+        link_name,
+        (SELECT count(1) as num_of_conn FROM efm_extension.pgpool_backendpid_details (link_name,pg_backend_pid()) LIMIT 1)
+    FROM
+        efm_extension.get_pgpool_links()
+)
+SELECT
+    CASE WHEN COUNT(1) > 0 THEN 
+      TRUE
+    ELSE 
+       pg_catalog.pg_is_in_recovery() 
+    END INTO recovery_status 
+FROM
+    pid_info where num_of_conn > 0;
+
+    PERFORM
+        dblink_disconnect (link_name)
+    FROM
+        efm_extension.get_pgpool_links();
+
+    RETURN recovery_status;
+
+ EXCEPTION
+    WHEN OTHERS THEN
+    PERFORM
+        dblink_disconnect (link_name)
+    FROM
+        efm_extension.get_pgpool_links();
+
+    RETURN recovery_status;
+
+END;
+$FUNCTION$;
+
+REVOKE ALL ON FUNCTION efm_extension.pg_is_in_recovery() FROM PUBLIC;
 
 CREATE OR REPLACE FUNCTION efm_extension.pg_last_xlog_replay_location() 
     RETURNS pg_lsn
@@ -105,3 +273,4 @@ SELECT
 END;
 $FUNCTION$;
 
+REVOKE ALL ON FUNCTION efm_extension.pg_last_xlog_replay_location() FROM PUBLIC;
